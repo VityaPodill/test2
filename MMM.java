f7dbaace11be39d26a22b9f0409029149b456e75
Viktor Churylo <Viktor.Churylo@firstgroup.com>
2025-03-29 20:15:58 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	MMM.java

--- MMM.java
+++ MMM.java
@@ -2,6 +2,6 @@ public class MMM {
     public static void main(String[] args) {
         int a = 1, b = 2;
         System.out.println(a + b);
-        int c = 33, d = 44;
+        int c = 33, d = 44, e = 5;
     }
 }
