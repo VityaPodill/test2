4b7f9d55b7bba9c8844b0ff03f24843b7b83cbf9
VityaPodill <119930971+VityaPodill@users.noreply.github.com>
2025-03-29 21:22:26 +0200

    Add j=9 from GitHub

--- MMM.java
+++ MMM.java
@@ -3,6 +3,6 @@ public class MMM {
         int a = 1, b = 2;
         System.out.println(a + b);
         int c = 33, d = 44, e = 5,
-                f = 6, g = 7, h = 888;
+                f = 6, g = 7, h = 888,j=9;
     }
 }
