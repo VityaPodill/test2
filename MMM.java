d5a9dc43fcd54bde1fb78848d5a983753491cb31
Viktor Churylo <Viktor.Churylo@firstgroup.com>
2025-03-29 17:18:24 +0200

    Update ReadMe sum_minus

--- MMM.java
+++ MMM.java
@@ -1,10 +1,13 @@
 public class MMM {
     public static void main(String[] args) {
-        System.out.println("fffsssssssf");int a=5,b=11,c;c=sum(a,b);
+        System.out.println("fffsssssssf");
+        int a = 5, b = 11, c;
+        c = sum(a, b);
         System.out.println(c);
         minus(a, b);
     }
-    private static int sum      (int a,int b) {
+
+    private static int sum(int a, int b) {
         return a + 2 * b;
 
 
