998232391381d9cd7d0215cea4f929a09a41f836
Viktor Churylo <Viktor.Churylo@firstgroup.com>
2025-03-29 17:09:07 +0200

    Update ReadMe 777

--- MMM.java
+++ MMM.java
@@ -1,5 +1,17 @@
 public class MMM {
     public static void main(String[] args) {
-        System.out.println("fffsssssssf");
+        System.out.println("fffsssssssf");int a=5,b=11,c;c=sum(a,b);
+        System.out.println(c);
+    }
+    private static int sum      (int a,int b) {
+        return a+b;
+
+
+
+
+
+
+
+
     }
 }
