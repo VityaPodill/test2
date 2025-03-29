ed9dfc81dbd5918fedf66da2bc4419f63d23d8c4
Viktor Churylo <Viktor.Churylo@firstgroup.com>
2025-03-29 20:27:35 +0200

    Added f=6

--- MMM.java
+++ MMM.java
@@ -2,6 +2,7 @@ public class MMM {
     public static void main(String[] args) {
         int a = 1, b = 2;
         System.out.println(a + b);
-        int c = 33, d = 44, e = 5;
+        int c = 33, d = 44, e = 5,
+                f = 6;
     }
 }
