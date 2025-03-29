dedff4c6710e316f6d524073346d3244ecdfe65f
Viktor Churylo <Viktor.Churylo@firstgroup.com>
2025-03-29 19:48:20 +0200

    Deleted almost everything

--- MMM.java
+++ MMM.java
@@ -1,30 +1,6 @@
 public class MMM {
     public static void main(String[] args) {
         int a = 12, b = 3;
-        System.out.println(sum(a, b));
-        System.out.println(sqrt(a + b));
-        minus(a, b);
-        System.out.println(square(a + b));
-        exp(a, b);
-    }
 
-    private static int sum(int a, int b) {
-        return a + b;
-    }
-
-    public static void minus(int a, int b) {
-        System.out.println(Math.abs(a - b));
-    }
-
-    private static double sqrt(int a) {
-        return Math.sqrt(a);
-    }
-
-    private static double square(int a) {
-        return (a * a);
-    }
-
-    private static void exp(int a, int b) {
-        System.out.println(Math.pow(a, b));
     }
 }
