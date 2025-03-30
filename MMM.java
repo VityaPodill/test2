25f7943c86deb10c03b46683a8410ca75986eacc
Viktor Churylo <Viktor.Churylo@firstgroup.com>
2025-03-30 10:19:18 +0300

    Added sum

--- MMM.java
+++ MMM.java
@@ -3,7 +3,8 @@ public class MMM {
         int a = 1, b = 2;
         System.out.println(a + b);
         int c = 33, d = 44, e = 5,
-                f = 6, g = 7, h = 888,j=9,k=10;
-        f=555
+                f = 6, g = 7, h = 888,
+                j = 9, k = 10;
+        System.out.println(f + g + h + j + k);
     }
 }
